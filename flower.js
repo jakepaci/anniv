7450d5bc5299e8b95a642167d44e7485b8af3cc8
jakepaci <124420899+jakepaci@users.noreply.github.com>
2024-09-06 13:05:10 +0800

    Update flower.js

--- flower.js
+++ flower.js
@@ -1,6 +1,5 @@
-onload = () => {
-  const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-    clearTimeout(c);
-  }, 1000);
-};
+const audio = new Audio('audio/kiss me.ogg');
+
+document.querySelector('.envelope-wrapper').addEventListener('click', () => {
+  audio.play();  // Trigger the audio playback when the user clicks on the envelope
+});
